5348a3a9a5374bd718d8522e261bb6d59a57a525
vjfernandez14 <134973199+vjfernandez14@users.noreply.github.com>
2023-10-07 16:15:16 -0300

    CreaTuLanding+Fernandez

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; //incluimos bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // incluimos bootstrap
 
-
-
-//importamos componentes
+// Importamos componentes
 import React from 'react';
 import NavBar from './components/NavBar';
 import Contenedor from './components/ItemListContainer';
 
-//app principal
+// App principal
 function App() {
- 
   return (
-  <div> 
-      <NavBar logoTienda="src\imagenes\logo1.png" />;
-      <Contenedor MensajeUsuario="Bienvenido, pronto podras ver nuestros productos" />;
-  </div>
-   
+    <div>
+      <NavBar logoTienda="src/imagenes/logo1.png" />
+      <Contenedor MensajeUsuario="Bienvenido, pronto podrás ver nuestros productos" />
+    </div>
   );
-  }
+}
 
 export default App;
+
